b0d5e8a3e6108a4a0eb1e8a4a9f077a036cae28c
Matt Jennings <matt@mattjennings.net>
2017-06-27 15:50:35 -0700

    middle of build jquery slideshow

--- js/global.js
+++ js/global.js
@@ -9,10 +9,17 @@
             var slideshowImgs = getSlideshow.find('img');
             var sum = 0; 
 
+            var previousSymbol = $('<a class="slideshow_mj_prev" href="#">&lt;</a>')
+            var nextSymbol = $('<a class="slideshow_mj_next" href="#">&lt;</a>')
+
+            previousSymbol.css('position', 'absolute');
+            
+            getSlideshow.prepend(previousSymbol);
+
+
             // Add data attributes to images
             getSlideshowLi.map(function() {
-                sum++;
-                return $(this).attr('data-num', sum);
+                
             });
 
         }
